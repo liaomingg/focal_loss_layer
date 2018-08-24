b8da6c2975ea5eb66f9dd813b0e28e8420d32197
JY92 <568814045@qq.com>
2018-08-24 10:53:27 +0800

    focal_loss_layer.cpp

--- focal_loss_layer.cpp
+++ focal_loss_layer.cpp
@@ -16,10 +16,10 @@ void FocalLossLayer<Dtype>::LayerSetUp(
   softmax_param.set_type("Softmax");
   softmax_layer_ = LayerRegistry<Dtype>::CreateLayer(softmax_param);
   softmax_bottom_vec_.clear();
-  softmax_bottom_vec_.push_back(bottom[0]);// softmax_bottom_vec_[0]指向bottom[0]
+  softmax_bottom_vec_.push_back(bottom[0]);
   softmax_top_vec_.clear();
-  softmax_top_vec_.push_back(&prob_);// soft_top_vec_[0]指向prob_
-  // 建立softmax_layer, bottom[0] 即为该层输入， top[0]为prob_
+  softmax_top_vec_.push_back(&prob_);
+
   softmax_layer_->SetUp(softmax_bottom_vec_, softmax_top_vec_);
 
   has_ignore_label_ =
@@ -36,8 +36,8 @@ void FocalLossLayer<Dtype>::LayerSetUp(
     normalization_ = this->layer_param_.loss_param().normalization();
     normalization_ = this->layer_param_.loss_param().normalization();
   }
-  	// 获取alpha 和 gamma参数
-  	alpha_ = this->layer_param_.focal_loss_param().alpha();
+  // get alpha and gamma
+  alpha_ = this->layer_param_.focal_loss_param().alpha();
 	gamma_ = this->layer_param_.focal_loss_param().gamma();
 }
 
@@ -47,7 +47,7 @@ void FocalLossLayer<Dtype>::Reshape(
   LossLayer<Dtype>::Reshape(bottom, top);
   softmax_layer_->Reshape(softmax_bottom_vec_, softmax_top_vec_);
   softmax_axis_ =
-      bottom[0]->CanonicalAxisIndex(this->layer_param_.softmax_param().axis()); // 指明在哪个轴上做softmax分类
+      bottom[0]->CanonicalAxisIndex(this->layer_param_.softmax_param().axis()); // classify at which axis
   outer_num_ = bottom[0]->count(0, softmax_axis_);
   inner_num_ = bottom[0]->count(softmax_axis_ + 1);
   CHECK_EQ(outer_num_ * inner_num_, bottom[1]->count())
@@ -60,27 +60,27 @@ void FocalLossLayer<Dtype>::Reshape(
     top[1]->ReshapeLike(*bottom[0]);
   }
 }
-// 获取归一化因子
+
 template <typename Dtype>
 Dtype FocalLossLayer<Dtype>::get_normalizer(
     LossParameter_NormalizationMode normalization_mode, int valid_count) {
-  Dtype normalizer; // 声明一个归一化因子normalizer
+  Dtype normalizer; 
   switch (normalization_mode) {
     case LossParameter_NormalizationMode_FULL:
-      normalizer = Dtype(outer_num_ * inner_num_); // label元素总个数
+      normalizer = Dtype(outer_num_ * inner_num_); 
       break;
     case LossParameter_NormalizationMode_VALID:
       if (valid_count == -1) {
         normalizer = Dtype(outer_num_ * inner_num_);
       } else {
-        normalizer = Dtype(valid_count);			// 归一化因子为有效元素的个数
+        normalizer = Dtype(valid_count);			
       }
       break;
     case LossParameter_NormalizationMode_BATCH_SIZE:
-      normalizer = Dtype(outer_num_);				// 归一化因子为batch_size
+      normalizer = Dtype(outer_num_);				
       break;
     case LossParameter_NormalizationMode_NONE:
-      normalizer = Dtype(1);						// 不指定归一化因子， 使用默认的归一化因子: 1
+      normalizer = Dtype(1);						
       break;
     default:
       LOG(FATAL) << "Unknown normalization mode: "
@@ -90,12 +90,11 @@ Dtype FocalLossLayer<Dtype>::get_normalizer(
   // particular loss in a multi-task setup. The max prevents NaNs in that case.
   return std::max(Dtype(1.0), normalizer);
 }
-// 前向传播 修改了
+
 template <typename Dtype>
 void FocalLossLayer<Dtype>::Forward_cpu(
     const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
   // The forward pass computes the softmax prob values.
-  // 先用softmax分类器来计算该点在各类别上的概率值prob属于[0, 1]
   softmax_layer_->Forward(softmax_bottom_vec_, softmax_top_vec_);
   const Dtype* prob_data = prob_.cpu_data();
   const Dtype* label = bottom[1]->cpu_data();
@@ -110,10 +109,10 @@ void FocalLossLayer<Dtype>::Forward_cpu(
       }
       DCHECK_GE(label_value, 0);
       DCHECK_LT(label_value, prob_.shape(softmax_axis_));
-	  // 单点loss = -log(p) 修改loss需要修改这里
+	  // loss = -log(p)
 	  const Dtype pk = prob_data[i * dim + label_value * inner_num_ + j];
 	  loss -= alpha_ * powf(1 - pk, gamma_) * log(std::max(pk, Dtype(FLT_MIN)));
-      ++count; // 统计有效元素的个数
+      ++count; // count elements.
     }// 
   }// per_num
   top[0]->mutable_cpu_data()[0] = loss / get_normalizer(normalization_, count);
@@ -121,27 +120,21 @@ void FocalLossLayer<Dtype>::Forward_cpu(
     top[1]->ShareData(prob_);
   }
 }
-// 反向传播 需要修改
+
 template <typename Dtype>
 void FocalLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
-	// bottom[0]->cpu_data(), 即为zk
-	// 对label进行反向传播
+	// bottom[0]->cpu_data(), zk
   if (propagate_down[1]) {
     LOG(FATAL) << this->type()
                << " Layer cannot backpropagate to label inputs.";
   }
-  // 对bottom[0]进行反向传播
   if (propagate_down[0]) {
-	// 获取bottom[0]的梯度数据指针
     Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
-	// 获取各点的概率值
     const Dtype* prob_data = prob_.cpu_data();
-	// 如果i ！= k，则diff = prob
+	// if i ！= k，then diff = prob
     //caffe_copy(prob_.count(), prob_data, bottom_diff);
     const Dtype* label = bottom[1]->cpu_data();
-	// 获取bottom[0]的cpu数据， 即zj
-	
 	// const Dtype* bottom_data = bottom[0]->cpu_data();
     int dim = prob_.count() / outer_num_;
     int count = 0;
@@ -160,14 +153,12 @@ void FocalLossLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
 			for (c = 0; c < label_value; ++c) {
 				const Dtype pj = std::max(prob_data[i * dim + c * inner_num_ + j], Dtype(FLT_MIN));
 				bottom_diff[i * dim + c * inner_num_ + j] = Dtype(-1 * alpha_ * (gamma_ * pow(1 - pk, gamma_ - 1) * pk * pj * log(pk) - pow(1 - pk, gamma_) * pj)); // j != k
-			    //bottom_diff[i * dim + c * inner_num_ + j] = Dtype (-1 * alpha_ * (gamma_ * powf(1 - pk, gamma_ - 1)* pk * pk * exp(bottom_data[i * dim + c * inner_num_ + j] - bottom_data[i * dim + label_value * inner_num_ + j]) * log(pk) - powf(1 - pk, gamma_) * pj) );// j != k
 			} // per_channel
 			bottom_diff[i * dim + label_value * inner_num_ + j] = Dtype (-1 * alpha_ * (-1 * gamma_ * powf(1 - pk, gamma_) * pk * log(pk) + powf(1 - pk, gamma_ + 1)));	// j = k
-			c = c + 1;
-			for (c; c < num_channel; ++c) {
+			c++;
+			for ( ; c < num_channel; ++c) {
 				const Dtype pj = std::max(prob_data[i * dim + c * inner_num_ + j], Dtype(FLT_MIN));
 				bottom_diff[i * dim + c * inner_num_ + j] = Dtype(-1 * alpha_ * (gamma_ * pow(1 - pk, gamma_ - 1) * pk * pj * log(pk) - pow(1 - pk, gamma_) * pj)); // j != k
-				//bottom_diff[i * dim + c * inner_num_ + j] = Dtype(-1 * alpha_ * (gamma_ * powf(1 - pk, gamma_ - 1) * pk * pk * exp(bottom_data[i * dim + c * inner_num_ + j] - bottom_data[i * dim + label_value * inner_num_ + j]) * log(pk) - powf(1 - pk, gamma_) * pj));// j != k
 			} // per_channel
         }
       }// per_h_w
